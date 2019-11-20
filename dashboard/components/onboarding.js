5bc76318f8cd5785a786a6a2fd68ea812f3c85eb
hdriqi <ra@evius.id>
2019-11-20 21:28:29 +0700

    only allow image in avatar

--- dashboard/components/onboarding.js
+++ dashboard/components/onboarding.js
@@ -344,7 +344,7 @@ const Onboarding = ({
 														<svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 															<path fillRule="evenodd" clipRule="evenodd" d="M6.93702 5.84538C7.00787 5.74688 7.08656 5.62631 7.18689 5.46372C7.22356 5.40431 7.32355 5.23934 7.39799 5.11653L7.4818 4.97841C8.31079 3.62239 8.91339 3 10 3H15V5H10C9.91327 5 9.6405 5.28172 9.1882 6.02159L9.11542 6.14154L9.11524 6.14183C9.04019 6.26566 8.93096 6.44589 8.88887 6.51409C8.76592 6.71332 8.66375 6.86988 8.56061 7.01326C8.11237 7.63641 7.66434 8 7 8H4C3.44772 8 3 8.44772 3 9V18C3 18.5523 3.44772 19 4 19H20C20.5523 19 21 18.5523 21 18V12H23V18C23 19.6569 21.6569 21 20 21H4C2.34315 21 1 19.6569 1 18V9C1 7.34315 2.34315 6 4 6H6.8162C6.84949 5.96194 6.8903 5.91033 6.93702 5.84538ZM17 8V6H19V4H21V6H23V8H21V10H19V8H17ZM12 18C9.23858 18 7 15.7614 7 13C7 10.2386 9.23858 8 12 8C14.7614 8 17 10.2386 17 13C17 15.7614 14.7614 18 12 18ZM12 16C13.6569 16 15 14.6569 15 13C15 11.3431 13.6569 10 12 10C10.3431 10 9 11.3431 9 13C9 14.6569 10.3431 16 12 16Z" fill="white"/>
 														</svg>
-															<input className="absolute cursor-pointer inset-0 opacity-0 w-full" type="file" onChange={(e) => updateAvatarUrl(e.target.files)} />
+															<input className="absolute cursor-pointer inset-0 opacity-0 w-full" type="file" accept="image/*" onChange={(e) => updateAvatarUrl(e.target.files)} />
 														</div>
 													</div>
 												</div>
